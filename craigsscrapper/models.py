dbda30d7213f39b07f741971f6a01c7a0aa25bd5
HabiburRahmanShamim <shamimmahmed04@gmail.com>
2020-08-16 20:28:10 +0600

    added static folder for css

--- craigsscrapper/models.py
+++ craigsscrapper/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class Search(models.Model):
     search = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now = True)
+
+    #When a search object will call it will return search string
+    def __str__(self):
+        return '{}'.format(self.search)
+
+    #Database table name change
+    class Meta:
+        verbose_name_plural = 'Searches'
