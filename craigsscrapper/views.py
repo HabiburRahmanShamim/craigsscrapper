d33fedd5f5ca593678b0788d2141b06f681b1642
HabiburRahmanShamim <shamimmahmed04@gmail.com>
2020-08-17 00:10:08 +0600

    linked with craigsList

--- craigsscrapper/views.py
+++ craigsscrapper/views.py
@@ -1,11 +1,20 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+from requests.compat import quote_plus
 
 # Create your views here.
+
+BASE_SITE_URL = 'https://newyork.craigslist.org/search/?query={}'
 
 def home(request):
     return render(request, 'index.html')
 
 def search(request):
     search_text = request.POST.get('search')
-    print(search_text)
+    final_url = BASE_SITE_URL.format(quote_plus(search_text))
+    print(final_url)
+    response = requests.get(final_url)
+    site_data = response.text
+    
     return render(request, 'search_list.html')
