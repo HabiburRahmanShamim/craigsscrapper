28ce82354389d62aac47fd3c0ecdd7c9d717c3ce
HabiburRahmanShamim <shamimmahmed04@gmail.com>
2020-08-17 00:38:42 +0600

    Successfully cooked data for my site

--- craigsscrapper/views.py
+++ craigsscrapper/views.py
@@ -6,6 +6,7 @@ from requests.compat import quote_plus
 # Create your views here.
 
 BASE_SITE_URL = 'https://newyork.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 def home(request):
     return render(request, 'index.html')
@@ -16,5 +17,37 @@ def search(request):
     print(final_url)
     response = requests.get(final_url)
     site_data = response.text
-    
-    return render(request, 'search_list.html')
+
+    soup = BeautifulSoup(site_data, features='html.parser')
+
+    posts = soup.find_all('li', {'class' : 'result-row'})
+
+    final_posts = []
+
+    for post in posts:
+        post_time = post.find(class_ = 'result-date')
+        post_title = post.find(class_ = 'result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_ = 'result-price'):
+            post_price = post.find(class_ = 'result-price').text
+        else :
+            post_price = 'N/A'
+        
+
+        if post.find(class_ = 'result-image').get('data-ids'):
+            post_image_id = post.find(class_ = 'result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_path = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_path = 'https://craigslist.org/images/peace.jpg'
+
+        final_posts.append((post_title, post_url, post_price, post_image_path))
+
+    #print(soup.find('a', {'class' : 'result-title'}).text) 
+
+    frontend_stuff = {
+        'search' : search_text,
+        'final_posts' : final_posts,
+    }
+    print(frontend_stuff)
+
+    return render(request, 'search_list.html', frontend_stuff)
